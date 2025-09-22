263ba94f46337ee6bc184862f95eebdea94c3df2
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-09-22 08:38:02 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Pattern12_StructuralProxy/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Pattern12_StructuralProxy
+{
+    public abstract class Subject
+    {
+        public abstract void Request();
+
+    }
+}
