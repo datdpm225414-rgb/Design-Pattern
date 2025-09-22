b009bd9a5c972af870f0c991177f149c4b0d285c
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-09-22 09:18:00 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Pattern13_ChainOfResponsibility/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Pattern13_ChainOfResponsibility
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            Handler h3 = new ConcreteHandler3();
+            h1.SetSuccessor(h2);
+            h2.SetSuccessor(h3);
+            h3.SetSuccessor(h1);
+
+            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+            foreach (int request in requests)
+            {
+                h1.HandleRequest(request);
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
