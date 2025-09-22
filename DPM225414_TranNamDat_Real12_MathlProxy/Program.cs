897e6b50aeb01cf0f348b6e2105c0cedb75626b8
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-09-22 08:58:34 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Real12_MathlProxy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Real12_MathlProxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathProxy proxy = new MathProxy();
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+            Console.ReadKey();
+        }
+    }
+}
