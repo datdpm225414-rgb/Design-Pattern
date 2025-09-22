12f78fae0940d39a416a84b317a2f4a0c14f55f3
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-09-22 09:54:49 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Real13_ChainOfResponsibility/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Real13_ChainOfResponsibility
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Approver larry = new Director();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+            Purchase p = new Purchase(2034, 350.00, "Supplies");
+            larry.ProcessRequest(p);
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DPM225414_TranNamDat_Real13_ChainOfResponsibility/Purchase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Real13_ChainOfResponsibility
+{
+    public class Purchase
+    {
+        int number;
+        double amount;
+        string purpose;
+        public Purchase(int number, double amount, string purpose)
+        {
+            this.number = number;
+            this.amount = amount;
+            this.purpose = purpose;
+        }
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+    }
+}
