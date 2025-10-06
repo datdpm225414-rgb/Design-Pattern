c07b5a96f1448bd14d351ed48768d7a727a21e36
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-06 10:12:40 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Pattern04_PrototypePattern/DPM225414_TranNamDat_Pattern04_PrototypePattern/ConcretePrototype1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Pattern04_PrototypePattern
+{
+    public class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id)
+    : base(id)
+        {
+        }
+        // Returns a shallow copy
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
---
+++ DPM225414_TranNamDat_Pattern04_PrototypePattern/DPM225414_TranNamDat_Pattern04_PrototypePattern/ConcretePrototype2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Pattern04_PrototypePattern
+{
+    public class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(string id)
+    : base(id)
+        {
+        }
+        // Returns a shallow copy
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+    }
+}
