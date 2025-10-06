1b3233dbde3bf6b5d054fa073cf19e488b683821
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-06 08:37:27 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Pattern14_ChainofResponsibility/DPM225414_TranNamDat_Pattern14_ChainofResponsibility/ConcreteHandler1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Pattern14_ChainofResponsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                    this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    
+    }
+}
